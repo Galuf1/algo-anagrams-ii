ab5e0758057d59bd3192995235598d9733720130
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 22:31:21 +0200

    comments

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,17 +1,14 @@
 exports.anagramsFor = function(word, listOfWords) {
+    // create a function to filter the listOfWords
     const filterAnagram = function(str1){
-        
+       // sort and compare to see if they are anagrams
        const sortedWord = word.split("").sort().join("")
        const sortedStr = str1.split("").sort().join("")
        return sortedWord === sortedStr
 
     }
-    
+    //filter creates a new list
     let result = listOfWords.filter(filterAnagram)
     return result
 };
 
-
-// listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
-
-// anagramsFor("threads",listOfWords)
--- python/anagram2.py
+++ python/anagram2.py
@@ -1,13 +1,17 @@
 def anagrams_for(word, list_of_words):
+		# create a function to filter if the iterator equals word
 		def filter_anagram(str1):
+			# create lists with string to be able to mutate them
 			list1 = list(word)
 			list2 = list(str1)
 			for i in list1:
 				if i in list2:
+					#remove the character from list2 if it is in word
 					list2.remove(i)
+			# should be equal if they match length and characters
 			if len(list2) == 0:
 				return True
-		
+		# filter using the list generates an iterator that has to be converted into a list
 		result_iterator = filter(filter_anagram,list_of_words )
 		result = list(result_iterator)
 

b24878dfce3c6e20edaf35869bf6805398fad0a8
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 21:59:07 +0200

    python with filter

--- python/anagram2.py
+++ python/anagram2.py
@@ -1,4 +1,14 @@
-# Don't forget to run the tests (and create some of your own)
-
 def anagrams_for(word, list_of_words):
-		# your code here
+		def filterAnagram(str1):
+			list1 = list(word)
+			list2 = list(str1)
+			for i in list1:
+				if i in list2:
+					list2.remove(i)
+			if len(list2) == 0:
+				return True
+		
+		result_iterator = filter(filterAnagram,list_of_words )
+		result = list(result_iterator)
+
+		return result

edf1fbc53db2d455557d4bd7932565a8c44cd8f0
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-05-27 22:25:52 +0200

    js solved with sort

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,17 @@
 exports.anagramsFor = function(word, listOfWords) {
+    const filterAnagram = function(str1){
+        
+       const sortedWord = word.split("").sort().join("")
+       const sortedStr = str1.split("").sort().join("")
+       return sortedWord === sortedStr
 
+    }
+    
+    let result = listOfWords.filter(filterAnagram)
+    return result
 };
+
+
+// listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
+
+// anagramsFor("threads",listOfWords)
--- python/anagram2.py
+++ python/anagram2.py
@@ -1,5 +1,5 @@
 def anagrams_for(word, list_of_words):
-		def filterAnagram(str1):
+		def filter_anagram(str1):
 			list1 = list(word)
 			list2 = list(str1)
 			for i in list1:
@@ -8,7 +8,7 @@ def anagrams_for(word, list_of_words):
 			if len(list2) == 0:
 				return True
 		
-		result_iterator = filter(filterAnagram,list_of_words )
+		result_iterator = filter(filter_anagram,list_of_words )
 		result = list(result_iterator)
 
 		return result
